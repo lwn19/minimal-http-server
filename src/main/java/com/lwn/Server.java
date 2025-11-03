5501396abd038d204bccd3227fcf246848c29549
Chema <josemariagonz1998@gmail.com>
2025-11-04 00:22:11 +0100

    feat : "add Server.java"
    This class is the entrypoint

---
+++ src/main/java/com/lwn/Server.java
@@ -0,0 +1,18 @@
+package com.lwn;
+
+import com.lwn.handlers.RootHandler;
+import com.lwn.handlers.TimeHandler;
+
+import java.io.IOException;
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+        Router router = new Router();
+        router.addRoute("/",new RootHandler());
+        router.addRoute("/time",new TimeHandler());
+
+        HttpServer server = new HttpServer(8080,router,8);
+        server.start();
+    }
+}
