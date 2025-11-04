dcaf2146f50dcaba9063c322cb520418151fd213
Chema <josemariagonz1998@gmail.com>
2025-11-04 01:27:49 +0100

    feat : "implement Logger.java in HttpServer.java"

--- src/main/java/com/lwn/HttpServer.java
+++ src/main/java/com/lwn/HttpServer.java
@@ -1,6 +1,7 @@
 package com.lwn;
 
 import com.lwn.handlers.HttpHandler;
+import com.lwn.logger.Logger;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -31,6 +32,10 @@ public class HttpServer {
     }
 
     public void handleClient (Socket client) {
+
+        long start = System.currentTimeMillis();
+        String clientIp = client.getInetAddress().getHostAddress();
+
         try (
                 BufferedReader in = new BufferedReader(
                         new InputStreamReader(
@@ -68,10 +73,14 @@ public class HttpServer {
             out.write(response.getBody());
             out.flush();
 
+            long elapsed = System.currentTimeMillis() - start;
+            Logger.info (method + " " + path + " (" + response.getStatus() + ") from" +
+                    clientIp + " in " + elapsed + "ms");
+
 
 
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            Logger.info("Error en cliente : " + clientIp + ": " + e.getMessage());
         }
     }
 }
